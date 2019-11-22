0b2332b0b7bdd27fc8c5e1bb936c1fc1525601e5
josip.gabric <josip.gabric@E3C15>
2019-11-22 11:11:30 +0100

    generate files and build fetch data

---
+++ TSP/TSPSolver.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace TSPSolver.TSP
+{
+    public class TSPSolving
+    {
+        public TSPSolving(string name, IReadOnlyList<int> optimaltour, IReadOnlyList<Location> cities)
+        {
+            if (string.IsNullOrWhiteSpace(name))
+            {
+                throw new ArgumentException("Name must not be null or whitespace!", nameof(name));
+            }
+
+            Name = name;
+            Optimaltour = optimaltour ?? throw new ArgumentNullException(nameof(optimaltour));
+            Cities = cities ?? throw new ArgumentNullException(nameof(cities));
+        }
+
+        public string Name { get; }
+        public IReadOnlyList<int> Optimaltour { get; }
+    
+        public IReadOnlyList<Location> Cities { get; }
+    }
+
+    public class Location
+    {
+        public Location(double x, double y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public double X { get;}
+        public double Y { get; }
+    }
+}

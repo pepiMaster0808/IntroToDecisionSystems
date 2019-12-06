b4fb60829b1471acb5d5f4f87de47e768334b439
pepiMaster0808 <gabric.josip@gmx.at>
2019-12-06 10:56:29 +0100

    neu gitti

---
+++ TSP/Utils.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TSPSolver.TSP
+{
+    public static class Utils
+    {
+        public static double GetDistance(IReadOnlyList<int> solution, IReadOnlyList<Location> cities)
+        {
+            //TODO ihr schafft das!!!
+        }
+    }
+}
